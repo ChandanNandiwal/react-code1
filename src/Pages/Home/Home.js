ed512d7401ffaa3bcae70b873c4a255d937bdc1c
ChandanNandiwal <chandannandiwal1602@gmail.com>
2023-03-17 20:45:28 +0530

    card.js update and Home.js error

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Header from '../../Components/Header';
+import Card from '../../Components/Card';
+import {Grid} from '@mui/material';
+import {datas} from './data';
+import { StyledGridItem } from './Styled';
+
 
 const Home =() => {
     return (
         <div>
             <Header/>
+            <Grid> {
+            datas && 
+            datas.map((data, index) => {
+                return (
+                    <StyledGridItem item key={index} sm={12} md={3}>
+                       <Card data={data} /> 
+                    </StyledGridItem>
+                )
+            })
+
+         }
+         </Grid>
+        
         </div>
+        //  console.log("data", data);
     )
 }
 
-export default Home;
+// export default Home;
